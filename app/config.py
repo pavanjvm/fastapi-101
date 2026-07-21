8cd9534b3c30c417e5d3e3e3cdee5506a0161f9b
pavanjvm <pavanprasad0506@gmail.com>
2026-07-21 17:05:06 +0530

    sessiomaker handoff

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
-from pydantic_settings import BaseSettings, SettiungsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file = ".env",
-        env_file_encoding = "utf=8",
+        env_file_encoding = "utf-8",
         extra = "ignore", 
     )
     database_url: str = "sqlite:///./ctrlteach.db"
--- app/db.py
+++ app/db.py
@@ -1,12 +1,17 @@
 from sqlalchemy import String, create_engine
-from sqlalchemy.orm import DeclarativeBase,Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase,Mapped, mapped_column, sessionmaker
 from app.config import settings
-
+from sqlalchemy.orm import Session
 engine = create_engine(
     settings.database_url,
     echo = True,
 )
 
 class Base(DeclarativeBase):
-    __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key= True)
+    pass
+class User(Base):
+    __tablename__= "users"
+    id:Mapped[int] = mapped_column(primary_key=True)
+
+def init_db() -> None:
+    Base.metadata.create_all(bind=engine)
